88e711124a1540e66a5d3c7ffda14eee3ba4978e
gavrodge <s1746414@sms.ed.ac.uk>
2021-01-26 22:57:09 +0000

    a bit of rejigging and .py for tester plot

---
+++ csar.py
@@ -0,0 +1,15 @@
+from csr import caesar
+import yt
+#import astroconda
+import h5py
+import matplotlib.pyplot as plt
+import math
+import numpy
+
+#objective: mass of each galaxy vs SFR
+# y axis SFR, x axis stellar mass
+infile='/home/s1746414/shp/csr/m100n1024_151.hdf5'
+obj=caesar.load(infile)
+#returns found galaxies and found halos
+plt.scatter(numpy.log10([i.mass for i in obj.galaxies]),numpy.log10([j.sfr for j in obj.galaxies]))
+plt.show()
