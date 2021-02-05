0dd8e4bb2bf7f75919676321ae97d9988ea0d981
gavrodge <s1746414@sms.ed.ac.uk>
2021-02-05 15:56:25 +0000

    atom sucks

--- csar.py
+++ csar.py
@@ -11,5 +11,17 @@ import numpy
 infile='/home/s1746414/shp/m100n1024_151.hdf5'
 obj=caesar.load(infile)
 #returns found galaxies and found halos
+fig=plt.figure()
+ax=plt.subplot()
+ax.set_title('SFR vs Stellar Mass')
+ax.set_xlabel('Stellar Mass (logged)')
+ax.set_ylabel("$\\mathrm{log}_{10}\\mathrm{M}_{*}$")
 plt.scatter(numpy.log10([i.mass for i in obj.galaxies]),numpy.log10([j.sfr for j in obj.galaxies]))
-plt.savefig("plotsaveanddisplay.png")
+plt.savefig("letmecheck.png")
+plt.show()
+
+id=[i.slist for i in obj.galaxies]
+hf=h5py.File('snap_m100n1024_151.hdf5','r')
+hf.keys()
+hf.get(obj.galaxies.slist)
+hf.close()
