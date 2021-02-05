7bcfa5310f60d174b0de256ffb81f5c8cf0e3e49
gavrodge <s1746414@sms.ed.ac.uk>
2021-02-05 02:21:36 +0000

    annotated

--- csar.py
+++ csar.py
@@ -8,8 +8,8 @@ import numpy
 
 #objective: mass of each galaxy vs SFR
 # y axis SFR, x axis stellar mass
-infile='/home/s1746414/shp/csr/m100n1024_151.hdf5'
+infile='/home/s1746414/shp/m100n1024_151.hdf5'
 obj=caesar.load(infile)
 #returns found galaxies and found halos
 plt.scatter(numpy.log10([i.mass for i in obj.galaxies]),numpy.log10([j.sfr for j in obj.galaxies]))
-plt.show()
+plt.savefig("plotsaveanddisplay.png")
