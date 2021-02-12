ebf25e60cd5af5da15dbba8ee29e47c278326f98
gavrodge <s1746414@sms.ed.ac.uk>
2021-02-12 14:35:30 +0000

    better plot

--- csar.py
+++ csar.py
@@ -8,26 +8,6 @@ import matplotlib.cm as cm
 import math
 import numpy as np
 
-
-#objective: mass of each galaxy vs SFR
-# y axis SFR, x axis stellar mass
-infile='/home/s1746414/shp/m100n1024_151.hdf5'
-obj=caesar.load(infile)
-#returns found galaxies and found halos
-fig=plt.figure(figsize=(12,10))
-ax=plt.subplot()
-ax.set_title('SFR vs Stellar Mass')
-ax.set_xlabel("$\\mathrm{log_{10}}\\mathrm{M_{\\star}}$")
-ax.set_ylabel("$\\mathrm{log_{10}}\\mathrm{SFR}$")
-#colormap=[cm.plasma(i) for i in np.linspace(0,0.9,len(ax.collections))]
-plt.scatter(np.log10([i.mass for i in obj.galaxies]),np.log10([j.sfr for j in obj.galaxies]))
-"""
-for i,j in enumerate(ax.collections):
-    i.set_color(colormap[j])
-"""
-#text.usetex(True)
-#text.latexpreview(True)
-#ax.linewidth(1.6)
 plt.rcParams['xtick.top']=True
 plt.rcParams['xtick.direction']='in'
 plt.rcParams['xtick.minor.visible']=True
@@ -42,6 +22,23 @@ plt.rcParams['ytick.major.size']=5.4
 plt.rcParams['ytick.minor.size']=3
 plt.rcParams['ytick.major.width']=1.6
 plt.rcParams['ytick.minor.width']=1.2
+
+#objective: mass of each galaxy vs SFR
+# y axis SFR, x axis stellar mass
+infile='/home/s1746414/shp/m100n1024_151.hdf5'
+obj=caesar.load(infile)
+#returns found galaxies and found halos
+fig=plt.figure(figsize=(12,10))
+ax=plt.subplot()
+ax.set_title('SFR vs Stellar Mass')
+ax.set_xlabel("$\\mathrm{log_{10}}\\mathrm{(M_{\\star})[M_{\\odot}]}$")
+ax.set_ylabel("$\\mathrm{log_{10}}\\mathrm{(SFR)[M_{\\odot}yr^{-1}]}$")
+
+x=np.log10([i.mass for i in obj.galaxies])
+y=np.log10([j.sfr for j in obj.galaxies])
+
+plt.scatter(x,y,s=0.1,c=np.random.rand(len(x),),cmap='hsv',linewidths=1.6)
+
 
 #plt.savefig("letmecheck.png")
 plt.show()
