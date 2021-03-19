7cd70d10223e033c0b86e932ca4a63e9c69b3b27
gavrodge <s1746414@sms.ed.ac.uk>
2021-03-19 00:31:34 +0000

    finally found the GPR call

--- sklevel.py
+++ sklevel.py
@@ -1,10 +1,37 @@
+import numpy as np
+import h5py
 from astropy.table import Table
 from astropy.io import fits
 import pandas as pd
 import sklearn.gaussian_process as gp
+#from sklearn.gaussian_process import GaussianProcessRegressor; from sklearn.gaussian_process.kernels import RBF
+import sklearn.gaussian_process._gpr
+import sklearn.gaussian_process.kernels
+#import gpflow
+#from gpflow.utilities import print_summary
 
+#read in the 2D SFR array
 skdata=Table.read("galaxys_sfrs.fits").to_pandas()
+#print(skdata[0,:])
+print(skdata['82'][42])
 
-kernel=gp.kernels.ConstantKernel(1.,(1e-1,1e3)) #covariance function
-gp.GaussianProgressRegressor(kernel=kernel)
-gp.kernels.RBF(10.,(1e-3,1e3))
+#define the covariance function
+kernel=gp.kernels.RBF(10.,(1e-3,1e3))#squared exponential kernel
+gpr=gp._gpr.GaussianProcessRegressor(kernel=kernel,X_train_=skdata,y_train_=np.arange(0,13.4,0.1))
+gprs=gpr.fit(X,y)
+geeprs=gprs.predict(X)
+gpers=gprs.sample_y(X)
+geepers=gprs.score(X,y)
+
+#another approach
+kernelRBF = gpflow.kernels.SquaredExponential()
+#ExpSineSquared seems to be an accurate and precise option
+
+array[:, 0]#SFRs for first galaxy over all time
+array[0, :]#galaxies at first timestep
+
+#X_train_=np.arange(0,13.4,0.1)
+#y_train_=(skdata[0,:],100)
+
+X_train_=skdata
+y_train_=np.arange(0,13.4,0.1)
