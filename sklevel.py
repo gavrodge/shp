7cd49809e9aef13e297834c904e7ef7ec2c79a06
gavrodge <s1746414@sms.ed.ac.uk>
2021-04-03 12:41:48 +0100

    final

--- sklevel.py
+++ sklevel.py
@@ -4,34 +4,64 @@ from astropy.table import Table
 from astropy.io import fits
 import pandas as pd
 import sklearn.gaussian_process as gp
-#from sklearn.gaussian_process import GaussianProcessRegressor; from sklearn.gaussian_process.kernels import RBF
-import sklearn.gaussian_process._gpr
-import sklearn.gaussian_process.kernels
-#import gpflow
-#from gpflow.utilities import print_summary
+from sklearn.gaussian_process._gpr import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+import matplotlib.pyplot as plt
+
 
 #read in the 2D SFR array
-skdata=Table.read("galaxys_sfrs.fits").to_pandas()
-#print(skdata[0,:])
-print(skdata['82'][42])
+skdat=Table.read("galaxys_sfrs.fits").to_pandas()
+skdata= np.array(skdata)
+#print(skdata[0,:])#SFRs for all galaxies at first timestep
+#print(skdata[:,0])#SFRs for first galaxy over all time
+time_bins=np.arange(0,13.8,0.1)
+
+
+
+X_tr=time_bins.reshape(-1,1)
+ys=[0,2,22]
+for i in ys:
+    y_tr=skdata[:,i]
+    kernel = gp.kernels.ConstantKernel(1, (1e-1, 1e3)) * gp.kernels.RBF(10, (1e-3, 1e3))
+    model = gp.GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha=0.1, normalize_y=True)
+    #y_tr=model.sample_y(X_tr)
+    model.fit(X_tr, y_tr)
+    params = model.kernel_.get_params()
+    y_pred, std = model.predict(X_tr, return_std=True)
+    MSE = ((y_pred-y_tr)**2).mean()
+    print(MSE)
+    plt.plot(X_tr,y_tr)
+#plt.plot(X_tr,y_pred)
+plt.show()
+
+
 
 #define the covariance function
-kernel=gp.kernels.RBF(10.,(1e-3,1e3))#squared exponential kernel
-gpr=gp._gpr.GaussianProcessRegressor(kernel=kernel,X_train_=skdata,y_train_=np.arange(0,13.4,0.1))
+kernel=gp.kernels.RBF(1,(1e-3,1e3))#+gp.kernels.RBF(0.1,(1e-3,1e3))*gp.kernels.ConstantKernel(0.1)#squared exponential kernel
+#RBF correlation times and boundaries
+gpr=gp._gpr.GaussianProcessRegressor(kernel=kernel,random_state=0)
+
+X=time_bins.reshape(-1,1)
+
+y=gpr.sample_y(X)
+#y= gpr.train_y_()
+
+for i in range(5):
+
+    y=gpr.sample_y(X, random_state=i)
+    y/=np.max(y)
+
+    plt.plot(X,10**y)
+
+plt.show()
+nsamples=skdata[0,:]
+ntargets=len(nsamples)
+y=skdata#np.array([[nsamples,ntargets]])
 gprs=gpr.fit(X,y)
-geeprs=gprs.predict(X)
-gpers=gprs.sample_y(X)
+geeprs,std=gprs.predict(X)
+
 geepers=gprs.score(X,y)
 
 #another approach
 kernelRBF = gpflow.kernels.SquaredExponential()
 #ExpSineSquared seems to be an accurate and precise option
-
-array[:, 0]#SFRs for first galaxy over all time
-array[0, :]#galaxies at first timestep
-
-#X_train_=np.arange(0,13.4,0.1)
-#y_train_=(skdata[0,:],100)
-
-X_train_=skdata
-y_train_=np.arange(0,13.4,0.1)
